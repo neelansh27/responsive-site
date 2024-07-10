642276e0f1249995bddb59b7115a88fb80465100
neelansh27 <neelanshsharma30@gmail.com>
2024-07-10 12:44:10 +0530

    mobile screen image fixes and font fixes

--- carousel.js
+++ carousel.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     items:1,
     loop:true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout:4000,
     nav:true,
   })
